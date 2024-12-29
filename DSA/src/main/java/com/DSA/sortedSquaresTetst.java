b8ffc02f37cd2348d8ada46c2eeb03d04ea443fb
Harshit Sharma <harshit9466@gmail.com>
2024-12-29 18:55:03 +0530

    DSA

---
+++ DSA/src/main/java/com/DSA/sortedSquaresTetst.java
@@ -0,0 +1,19 @@
+package com.DSA;
+
+import java.util.Arrays;
+
+class sortedSquaresTetst
+{
+	public int[] sortedSquares(int[] nums)
+	{
+		int tempNum[] = new int[nums.length];
+
+		for(int i = 0; i < nums.length; i++)
+		{
+			int num = nums[i];
+			tempNum[i] = num * num;
+		}
+		Arrays.sort(tempNum);
+		return tempNum;
+	}
+}
---
+++ yt/StoneWeights.java
@@ -0,0 +1,7 @@
+package com.DSA2;
+class stoneWeights {
+    public int stoneWeights(int[][] stones) {
+        // write your code here
+    	return 0;
+    }
+}
